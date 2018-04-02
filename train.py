3271e0baf12a1f0af068f859e3765bf7b0ab3075
footh <jeff@faath.com>
2018-04-01 22:33:01 -0600

    valid loss streak

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ TRAINING_STEPS = [8000, 4000, 4000, 4000]
 # For discrete learning rates
 LEARNING_RATES = [0.002, 0.001, 0.0007, 0.0003]
 # For exponential decay learning rate based on valid loss progress
-VALID_LOSS_STREAK_MAX = [10, 8, 7, 7, 6, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3]
+VALID_LOSS_STREAK_MAX = [8, 8, 9, 10, 10, 10, 10, 10, 10]
 EXPONENTIAL_DECAY_BASE = 0.8
 LEARNING_RATE_BASE = 0.001
 NEAR_LOSS_TOLERANCE = 0.0017
